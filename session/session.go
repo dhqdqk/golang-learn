036e70677fa08c06688c5dae74c34fe14936c7ba
duanhq <690200292@qq.com>
2016-06-07 09:53:06 +0800

    session

---
+++ session/session.go
@@ -0,0 +1,22 @@
+package session
+
+import (
+	"fmt"
+	"sync"
+)
+
+// golobal session manager
+type Manager struct {
+	cookieName  string     // private cookiename
+	lock        sync.Mutex // protects session
+	provider    Provider
+	maxlifetime int64
+}
+
+func NewManager(provideName, cookieName string, maxlieftime int64) (*Manager, error) {
+	provider, ok := provides[provideName]
+	if !ok {
+		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
+	}
+	return &Manager{provider: provider, cookieName: oookieName, maxlifetime: maxlifetime}, nil
+}
